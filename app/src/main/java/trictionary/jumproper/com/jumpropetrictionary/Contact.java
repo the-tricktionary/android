49453dc3b4b2b479958346089ab42e2db5b4f045
jumproper <dylanmplummer@gmail.com>
2016-09-06 15:11:13 -0400

    Contact form follows correct db structure and includes user name and trick name

--- app/src/main/java/trictionary/jumproper/com/jumpropetrictionary/Contact.java
+++ app/src/main/java/trictionary/jumproper/com/jumpropetrictionary/Contact.java
@@ -7,16 +7,48 @@ public class Contact {
     private String name;
     private String type;
     private String desc;
+    private String[]replies;
 
     public Contact(){
         this.name="Test";
         this.type="General";
         this.desc="This is a test";
+        replies=new String[0];
     }
 
     public Contact(String name, String type, String desc) {
-        this.name = name;
+        if(name.length()>0) {
+            this.name = name;
+        }
+        else{
+            this.name="Anonymous";
+        }
         this.type = type;
+        this.desc = desc;
+        replies=new String[0];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
         this.desc = desc;
     }
 }
