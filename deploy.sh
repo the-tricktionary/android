e6871f1cdde88cb52644be9e6bedd3362bcf7246
Svante Bengtson <svante.b37@gmail.com>
2016-09-23 22:36:52 +0200

    fancier names

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git clone -b master git@github.com:the-tricktionary/the-tricktionary.github.io.g
 cd .deploy || exit 1
 git config user.name travis
 git config user.email travis@nowhere
-cp -R ../app/build/outputs/apk/app-debug.apk "./apps/app-${TRAVIS_BRANCH//\/}.apk"
+cp -R ../app/build/outputs/apk/app-debug.apk "./apps/app-${TRAVIS_BRANCH/\//-}.apk"
 git add -A
 git commit -m "travis: ${TRAVIS_COMMIT}"
 git push || exit 1
