d2ee25c41e82c65bf88c2cbdffae72536cc1cc0e
jumproper <jumproper36@gmail.com>
2016-12-26 13:51:28 -0500

    All stats done

--- app/src/main/java/trictionary/jumproper/com/jumpropetrictionary/Stats.java
+++ app/src/main/java/trictionary/jumproper/com/jumpropetrictionary/Stats.java
@@ -1,6 +1,7 @@
 package trictionary.jumproper.com.jumpropetrictionary;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,8 +16,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Stats extends AppCompatActivity {
-    private TextView numTricks;
-    int numTricksCount=0;
+    private TextView numTricks,numLevel1Tricks,numLevel2Tricks,numLevel3Tricks,numLevel4Tricks,averageTricks,maxTricks;
+    private int numTricksCount,numLevel1Count,numLevel2Count,numLevel3Count,numLevel4Count,numUsers,maxCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +27,59 @@ public class Stats extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         numTricks=(TextView)findViewById(R.id.num_tricks);
+        numLevel1Tricks=(TextView)findViewById(R.id.num_level_1_tricks);
+        numLevel2Tricks=(TextView)findViewById(R.id.num_level_2_tricks);
+        numLevel3Tricks=(TextView)findViewById(R.id.num_level_3_tricks);
+        numLevel4Tricks=(TextView)findViewById(R.id.num_level_4_tricks);
+        averageTricks=(TextView)findViewById(R.id.average_tricks);
+        maxTricks=(TextView)findViewById(R.id.max_tricks);
+
 
         FirebaseDatabase fb=FirebaseDatabase.getInstance();
         DatabaseReference myRef=fb.getReference("checklist");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                numTricksCount=0;
+                numLevel1Count=0;
+                numLevel2Count=0;
+                numLevel3Count=0;
+                numLevel4Count=0;
+                numUsers=0;
+                maxCount=0;
+                int count=0;
                 for(DataSnapshot ids:dataSnapshot.getChildren()){
+                    numUsers++;
                     for(DataSnapshot id0:ids.getChildren()){
                         for(DataSnapshot tricks:id0.getChildren()){
+                            count++;
+                            if(Integer.parseInt(id0.getKey().toString())==0){
+                                numLevel1Count++;
+                            }
+                            if(Integer.parseInt(id0.getKey().toString())==1){
+                                numLevel2Count++;
+                            }
+                            if(Integer.parseInt(id0.getKey().toString())==2){
+                                numLevel3Count++;
+                            }
+                            if(Integer.parseInt(id0.getKey().toString())==3){
+                                numLevel4Count++;
+                            }
                             numTricksCount++;
                         }
+                        if(count>maxCount){
+                            maxCount=count;
+                        }
+                        count=0;
                     }
                 }
                 numTricks.setText(""+numTricksCount);
+                numLevel1Tricks.setText(""+numLevel1Count);
+                numLevel2Tricks.setText(""+numLevel2Count);
+                numLevel3Tricks.setText(""+numLevel3Count);
+                numLevel4Tricks.setText(""+numLevel4Count);
+                averageTricks.setText(""+(numTricksCount/numUsers));
+                maxTricks.setText(""+maxCount);
             }
 
             @Override
@@ -47,6 +88,9 @@ public class Stats extends AppCompatActivity {
             }
         });
 
+    }
+    public void back(View v){
+        finish();
     }
 
 }
