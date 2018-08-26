8bc591976daf870515753d5c49a21bdbba09a8ba
jumproper <dylanmplummer@gmail.com>
2018-08-26 11:06:01 -0400

    Fix deploy.sh to find correct apk

---
+++ app/src/main/java/trictionary/jumproper/com/jumpropetrictionary/networkgraph/beans/Dimension.java
@@ -0,0 +1,56 @@
+package trictionary.jumproper.com.jumpropetrictionary.networkgraph.beans;
+
+/**
+ * The type Dimension.
+ */
+public class Dimension {
+
+    int width;
+
+    int height;
+
+    /**
+     * Instantiates a new Dimension.
+     *
+     * @param w the w
+     * @param h the h
+     */
+    public Dimension(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    /**
+     * Gets width.
+     *
+     * @return the width
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Gets height.
+     *
+     * @return the height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Equals boolean.
+     *
+     * @param o the o
+     * @return the boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Dimension) {
+            Dimension other = (Dimension) o;
+            return (width == other.width) && (height == other.height);
+        }
+
+        return false;
+    }
+}
