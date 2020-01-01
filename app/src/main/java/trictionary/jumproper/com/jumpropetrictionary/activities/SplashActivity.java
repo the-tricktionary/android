6adff73646e6620867bd9291153bed6401b35ca0
Dylan Plummer <dylanmplummer@gmail.com>
2020-01-01 17:01:09 -0500

    Update minSDKVersion to 24 and fix issues integrating FireStore

--- app/src/main/java/trictionary/jumproper/com/jumpropetrictionary/activities/SplashActivity.java
+++ app/src/main/java/trictionary/jumproper/com/jumpropetrictionary/activities/SplashActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.transition.Fade;
 import android.util.Log;
@@ -22,7 +24,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.internal.NonNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
